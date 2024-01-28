92d80b59486272b15e16d114162fc95e684c6571
ivan <ivan_a_kalinin@mail.ru>
2024-01-28 16:49:54 +0300

    added linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
+
 from brain_games import cli
 
+
 def greet():
-    print(f'Welcome to the Brain Games!')
+    greeting = 'Welcome to the Brain Games!'
+    print(f'{greeting}')
+
 
 def main():
     greet()
     cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
