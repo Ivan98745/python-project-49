fd7df52b48e0e3344306491a652b98ec8fc1d0ed
ivan <ivan_a_kalinin@mail.ru>
2024-01-28 14:21:22 +0300

    Adding third-party libraries

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from brain_games import cli
+
 def greet():
     print(f'Welcome to the Brain Games!')
 
 def main():
     greet()
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
